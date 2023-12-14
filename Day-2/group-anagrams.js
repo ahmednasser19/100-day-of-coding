e2549ec52eda7d6d0793019ad7b0c95306e9a360
Ahmed Nasser <ahmednas197@gmail.com>
2023-12-14 14:53:00 +0200

    Day-2 Group Anagram

---
+++ Day-2/group-anagrams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let groupAnagram = new Map();
+
+    for (let i = 0; i < strs.length; i++) {
+        let sortedWord = strs[i].split("").sort().join("")
+        if (groupAnagram.has(sortedWord)) {
+            groupAnagram.get(sortedWord).push(strs[i]);
+        } else {
+            groupAnagram.set(sortedWord, [strs[i]])
+        }
+    }
+
+    return Array.from(groupAnagram.values())
+
+};
+
+
+
