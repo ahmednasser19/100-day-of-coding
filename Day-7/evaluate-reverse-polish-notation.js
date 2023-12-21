90ea8dfcc9f15a747ca607868dd0f3e9147f53d2
Ahmed Nasser <ahmednas197@gmail.com>
2023-12-21 20:06:11 +0200

    Day 7 Eveluate revers polish notation

--- Day-7/evaluate-reverse-polish-notation.js
+++ Day-7/evaluate-reverse-polish-notation.js
@@ -5,33 +5,24 @@
 
 var evalRPN = function (tokens) {
     let result = [];
+    let newVal = 0;
     for (let i = 0; i < tokens.length; i++) {
-        if (tokens[i] === "+") {
-            let newVal = Number(result.pop()) + Number(result.pop())
-            result.push(newVal)
-        }
-        else if (tokens[i] === "*") {
-            let newVal = Number(result.pop()) * Number(result.pop())
-            result.push(newVal)
-        }
-        else if (tokens[i] === "-") {
-            let num1 = Number(result.pop())
-            let num2 = Number(result.pop())
-            let newVal = Math.floor(num2 - num1)
-            result.push(newVal)
-        }
-        else if (tokens[i] === "/") {
-            let num1 = Number(result.pop())
-            let num2 = Number(result.pop())
-            let newVal = Math.trunc(num2 / num1)
-            result.push(newVal)
-        }
 
+        if (tokens[i] === "+" || tokens[i] === "*" || tokens[i] === "-" || tokens[i] === "/") {
+            let num1 = Number(result.pop())
+            let num2 = Number(result.pop())
+
+            tokens[i] === "+" ? newVal = num1 + num2 :
+                tokens[i] === "*" ? newVal = num1 * num2 :
+                    tokens[i] === "-" ? newVal = num2 - num1 :
+                        newVal = Math.trunc(num2 / num1)
+
+            result.push(newVal)
+        }
         else {
             result.push(tokens[i])
         }
     }
     return result
 };
-
 
